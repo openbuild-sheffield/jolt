a7387b80fdb01c1e526b3a2ac7c74f6bb426b2b8
Danny Lewis <dannylewis.sheffield@googlemail.com>
2017-01-18 15:32:14 +0000

    Structures for mysql connection parameters

---
+++ Sources/OpenbuildMysql/struct.MySQLConnectionParams.swift
@@ -0,0 +1,7 @@
+// Create a structure for a MySQL connection params
+// https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/ClassesAndStructures.html#//apple_ref/doc/uid/TP40014097-CH13-ID82
+public struct MySQLConnectionParams {
+    public var host: String
+    public var username: String
+    public var password: String
+}
