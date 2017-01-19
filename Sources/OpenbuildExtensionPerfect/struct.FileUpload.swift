8390bab0994a98c473aae878d365fd957cf8f6ae
Danny Lewis <dannylewis.sheffield@googlemail.com>
2017-01-19 11:05:58 +0000

    Structure for file uploads

---
+++ Sources/OpenbuildExtensionPerfect/struct.FileUpload.swift
@@ -0,0 +1,9 @@
+public struct FileUpload {
+
+    public var fieldName: String
+    public var contentType: String
+    public var fileName: String
+    public var fileSize: Int
+    public var tmpFileName: String
+
+}
