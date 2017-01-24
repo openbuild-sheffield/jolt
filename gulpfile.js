db4ca8d161310f8fc0fd68b4b549d6235ac0e9af
Danny Lewis <dannylewis.sheffield@googlemail.com>
2017-01-24 12:31:36 +0000

    Added node package, used for building web assests

---
+++ gulpfile.js
@@ -0,0 +1,4 @@
+// all gulp tasks are located in the ./BuildWeb/tasks directory
+// gulp configuration is in files in ./BuildWeb directory
+var requireDir = require('require-dir');
+requireDir('WebBuild/tasks');
