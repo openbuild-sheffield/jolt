89fd08e689a178144103c62e74d9ccc6bad5e769
Danny Lewis <dannylewis.sheffield@googlemail.com>
2017-01-25 10:31:40 +0000

    Added file to define paths for build scripts

---
+++ WebBuild/paths.js
@@ -0,0 +1,12 @@
+var path = require('path');
+
+var appRoot = 'WebSource/';
+var outputRoot = 'Public/';
+
+module.exports = {
+  root: appRoot,
+  javascript: appRoot + 'javascript/**/*.js',
+  style: appRoot + 'sass/**/*.sass',
+  output: outputRoot,
+  sourceMapRelativePath: '../' + appRoot
+};
