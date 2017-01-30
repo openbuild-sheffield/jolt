3138d9b17184ad721238df37e2263de11e54d14e
Danny Lewis <dannylewis.sheffield@googlemail.com>
2017-01-30 10:43:55 +0000

    Added path of file to output javascript files installed via jspm

--- WebBuild/paths.js
+++ WebBuild/paths.js
@@ -6,6 +6,7 @@ var outputRoot = 'Public/';
 module.exports = {
   root: appRoot,
   javascript: appRoot + 'javascript/**/*.js',
+  javascriptLib: appRoot + 'javascript/_libs.js',
   style: appRoot + 'sass/**/*.sass',
   output: outputRoot,
   sourceMapRelativePath: '../' + appRoot,
