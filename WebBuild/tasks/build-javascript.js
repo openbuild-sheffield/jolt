c874b8557197bfbf165c3216d8a95e1a3cee6c11
Danny Lewis <dannylewis.sheffield@googlemail.com>
2017-02-03 10:31:33 +0000

    Updated paths

--- WebBuild/tasks/build-javascript.js
+++ WebBuild/tasks/build-javascript.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var paths = require('../paths');
 
 gulp.task('build-javascript', function () {
-    return gulp.src(paths.javascript)
+    return gulp.src([paths.root + 'javascript/_libs.js', paths.root + 'javascript/site/**/*.js'])
         .pipe(concat('app.js'))
         .pipe(gulp.dest(paths.output + 'javascript'));
 });
